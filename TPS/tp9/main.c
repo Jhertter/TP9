50242f17242adda115207ff598680468ca84f1d2
Ivan <jhertter@itba.edu.ar>
2021-05-12 09:58:08 -0300

    Tp9 Version 1

---
+++ TPS/tp9/main.c
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: ivan
+ *
+ * Created on May 12, 2021, 9:56 AM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) 
+{
+    int i = 0;
+    return (EXIT_SUCCESS);
+}
+
